d48b3d3d1c88fd659e56579b98450297300c1de5
Chad Fraser <chadfraser@Chads-MacBook-Pro.local>
2019-08-30 11:28:19 -0600

    Add hover style to button

--- js/board-button.js
+++ js/board-button.js
@@ -4,6 +4,7 @@ function createButtons() {
     let buttonsBlock = document.getElementById("boards");
     let createNewBoardButton = document.createElement("button");
     createNewBoardButton.id = "btn-create-new-board";
+    createNewBoardButton.textContent = "Create a new board..."
     buttonsBlock.appendChild(createNewBoardButton);
 
     let savedBoards = Cookies.getJSON("savedBoards");
