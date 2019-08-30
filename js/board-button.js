9fbd188978a3d82b185c7abe6f5328408ec62383
Chad Fraser <chadfraser@Chads-MacBook-Pro.local>
2019-08-30 11:36:31 -0600

    Add onclick event to main button

--- js/board-button.js
+++ js/board-button.js
@@ -5,6 +5,30 @@ function createButtons() {
     let createNewBoardButton = document.createElement("button");
     createNewBoardButton.id = "btn-create-new-board";
     createNewBoardButton.textContent = "Create a new board..."
+    createNewBoardButton.addEventListener("click", function() {
+        this.textContent = "Creating a board";
+        this.classList.add("active");
+        let closeIcon = document.createElement("span");
+        closeIcon.textContent = "X";
+        this.appendChild(closeIcon);
+
+        this.appendChild(document.createElement("hr"));
+        let namingText = document.createElement("h5");
+        namingText.textContent = "What shall we call the board?"
+        let inputBox = document.createElement("input");
+
+        this.appendChild(namingText);
+        this.appendChild(inputBox);
+
+        let confirmCancelButtons = document.createElement("div");
+        let cancelButton = document.createElement("button");
+        cancelButton.id = "btn-cancel";
+        let confirmButton = document.createElement("button");
+        confirmButton.id = "btn-confirm";
+        confirmCancelButtons.appendChild(cancelButton);
+        confirmCancelButtons.appendChild(confirmButton);
+    })
+
     buttonsBlock.appendChild(createNewBoardButton);
 
     let savedBoards = Cookies.getJSON("savedBoards");
