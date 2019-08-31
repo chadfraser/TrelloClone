b97737bcb87deccaa5f7899f65e9321d6809e24a
Chad Fraser <chadfraser@Chads-MacBook-Pro.local>
2019-08-30 18:35:11 -0600

    Split main function

--- js/board-button.js
+++ js/board-button.js
@@ -1,25 +1,63 @@
-window.onload = createBoards;
+// window.onload = createBoards;
+const createNewBoardCard = document.getElementById("card-create-new-board");
 const optionButtons = document.createElement("div");
 const cancelButton = document.createElement("button");
 const createButton = document.createElement("button");
+const namingText = document.createElement("h5");
+const inputBox = document.createElement("input");
 
-cancelButton.textContent = "CANCEL";
-cancelButton.id = "btn-cancel";
-createButton.textContent = "CREATE";
-createButton.id = "btn-create";
-optionButtons.appendChild(cancelButton);
-optionButtons.appendChild(createButton);
+initializeOptionButtons();
+namingText.textContent = "What shall we call the board?"
+createNewBoardCard.addEventListener("click", toggleCard);
+
+function initializeOptionButtons() {
+    cancelButton.textContent = "CANCEL";
+    cancelButton.id = "btn-cancel";
+    createButton.textContent = "CREATE";
+    createButton.id = "btn-create";
+    optionButtons.appendChild(cancelButton);
+    optionButtons.appendChild(createButton);
+}
+
+function toggleCard() {
+    if (this.classList.contains("active")) {
+        reduceCard(this);
+    }
+    // } else {
+    //     expandCard(this);
+    // }
+}
+
+function expandCard(card) {
+    card.textContent = "Creating a board";
+    card.classList.add("active");
+    card.appendChild(closeIcon);
+    card.appendChild(document.createElement("hr"));
+    card.appendChild(namingText);
+    card.appendChild(inputBox);
+    card.appendChild(optionButtons);
+}
+
+function reduceCard(card) {
+    card.classList.remove("active");
+    while (card.firstChild) {
+        card.removeChild(card.firstChild);
+    }
+    card.textContent = "Create a new board...";
+}
+
+
 let closeIcon = document.createElement("span");
 closeIcon.textContent = "X";
 closeIcon.addEventListener("onclick", function(){
-    
+
 })
 
 function createBoards() {
-    let boardsBlock = document.getElementById("boards");
-    let createNewBoardCard = document.createElement("div");
-    createNewBoardCard.id = "card-create-new-board";
-    createNewBoardCard.textContent = "Create a new board..."
+    // let boardsBlock = document.getElementById("boards");
+    // let createNewBoardCard = document.createElement("div");
+    // createNewBoardCard.id = "card-create-new-board";
+    // createNewBoardCard.textContent = "Create a new board..."
     createNewBoardCard.addEventListener("click", function() {
         
         this.textContent = "Creating a board";
