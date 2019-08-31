b140becd1ad1362e7fa9e3b6578ec7d2018f33df
Chad Fraser <chadfraser@Chads-MacBook-Pro.local>
2019-08-30 20:53:15 -0600

    Add styling and initial object creation

---
+++ js/new-list-button.js
@@ -0,0 +1,25 @@
+const lists = document.getElementById("lists");
+const createNewListCard = document.getElementById("card-create-list");
+const newListInputDiv = document.createElement("div");
+newListInputDiv.id = "new-list-input-div";
+const newListInputBox = document.createElement("input");
+newListInputBox.placeholder = "add a list";
+const newListCloseIcon = document.createElement("img");
+newListCloseIcon.src = "img/iconmonstr-x-mark-aquamarine-opaque.png"
+newListCloseIcon.id = "new-list-close-icon";
+const nameInsistenceText = document.createElement("h7");
+
+newListInputDiv.appendChild(newListCloseIcon);
+newListInputDiv.appendChild(newListInputBox);
+newListInputDiv.appendChild(nameInsistenceText);
+
+createNewListCard.addEventListener("click", replaceNewListCard);
+
+function replaceNewListCard() {
+    lists.removeChild(createNewListCard);
+    lists.appendChild(newListInputDiv);
+}
+
+function foo() {
+
+}
