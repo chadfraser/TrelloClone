b64369bd131cfcefcc4c44367e90e096ff7956d8
Chad Fraser <chadfraser@Chads-MacBook-Pro.local>
2019-09-01 12:12:58 -0600

    Add checkmark css and event listener

--- js/new-list-button.js
+++ js/new-list-button.js
@@ -72,7 +72,6 @@ function createNewList(title, initializingLists) {
         }
     });
 
-    // listContainer.appendChild(newList);
     listContainer.appendChild(subcontainer);
     subcontainer.appendChild(newList);
     newList.appendChild(newListSubDiv);
@@ -89,11 +88,28 @@ function createNewList(title, initializingLists) {
 
 function createNewTask(title, mainDiv) {
     let newTask = document.createElement("div");
-    newTask.textContent = title;
     newTask.classList.add("task");
 
+    let taskText = document.createElement("p");
+    taskText.textContent = title;
+
     let checkmark = document.createElement("img");
-    checkmark.src = "img/iconmonstr-check-mark.png"
+    checkmark.src = "img/iconmonstr-check-mark.png";
+    checkmark.addEventListener("click", function() {
+        toggleCheckedTask(newTask);
+    });
+
+    newTask.appendChild(taskText);
     newTask.appendChild(checkmark);
     mainDiv.appendChild(newTask);
+}
+
+function toggleCheckedTask(task) {
+    if (task.classList.contains("checked")) {
+        task.classList.remove("checked");
+        this.classList.remove("checked");
+    } else {
+        task.classList.add("checked");
+        this.classList.add("checked");
+    }
 }
