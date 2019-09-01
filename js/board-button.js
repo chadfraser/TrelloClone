2cb91d9dadd3e456cb5fa6361ac6a27532708155
Chad Fraser <chadfraser@Chads-MacBook-Pro.local>
2019-09-01 15:23:43 -0600

    Refactor for clarity and simplicity

--- js/board-button.js
+++ js/board-button.js
@@ -1,68 +1,32 @@
 window.onload = createBoards;
 
-const deleteImage = document.getElementById("main-image-icon");
-deleteImage.addEventListener("click", function() {
-    let answer = confirm("Erase all created boards and tasks?");
-    if (answer) {
-        localStorage.clear();
-        while (cardContainer.childNodes.length > 2) {
-            cardContainer.removeChild(cardContainer.lastChild);
-        }
-    }
-});
-
+const deleteIcon = document.getElementById("main-image-icon");
 const cardContainer = document.getElementById("boards");
 const createNewBoardCard = document.getElementById("card-create-new-board");
 const mainCardHeader = document.createElement("div"); 
-mainCardHeader.classList.add("card-header");
-
 const boardCreationDiv = document.createElement("div");
 const optionButtons = document.createElement("div");
 const cancelButton = document.createElement("button");
 const createButton = document.createElement("button");
 const namingText = document.createElement("h5");
 const inputBox = document.createElement("input");
-inputBox.addEventListener("keydown", function() {
-    if (event.key === "Enter" && inputBox.value.trim() !== "") {
-        createNewBoard(inputBox.value, false);
-        inputBox.value = "";
-        reduceCard(createNewBoardCard);
-        event.stopPropagation();
-    }
-})
+const closeIcon = document.createElement("img");
 
-initializeOptionButtons();
 namingText.textContent = "What shall we call the board?"
-createNewBoardCard.addEventListener("click", toggleCard);
 
 boardCreationDiv.appendChild(namingText);
 boardCreationDiv.appendChild(inputBox);
 boardCreationDiv.appendChild(optionButtons);
 
-const closeIcon = document.createElement("img");
 closeIcon.classList.add("close-icon");
 closeIcon.src = "img/iconmonstr-x-mark.png";
+
+mainCardHeader.classList.add("card-header");
 mainCardHeader.textContent = "Creating a board";
 mainCardHeader.appendChild(closeIcon);
 
-closeIcon.addEventListener("click", function(){
-    reduceCard(createNewBoardCard);
-    inputBox.value = "";
-    event.stopPropagation();
-});
-cancelButton.addEventListener("click", function(){
-    reduceCard(createNewBoardCard);
-    inputBox.value = "";
-    event.stopPropagation();
-});
-createButton.addEventListener("click", function() {
-    if (inputBox.value.trim() !== "") {
-        createNewBoard(inputBox.value, false);
-        inputBox.value = "";
-        reduceCard(createNewBoardCard);
-        event.stopPropagation();
-    }
-})
+initializeOptionButtons();
+initializeEventListeners();
 
 function initializeOptionButtons() {
     cancelButton.textContent = "CANCEL";
@@ -72,6 +36,40 @@ function initializeOptionButtons() {
     optionButtons.appendChild(cancelButton);
     optionButtons.appendChild(createButton);
     optionButtons.id = "option-buttons";
+}
+
+function initializeEventListeners() {
+    deleteIcon.addEventListener("click", function() {
+        let answer = confirm("Erase all created boards and tasks?");
+        if (answer) {
+            localStorage.clear();
+            while (cardContainer.childNodes.length > 2) {
+                cardContainer.removeChild(cardContainer.lastChild);
+            }
+        }
+    });
+
+    inputBox.addEventListener("keydown", function() {
+        if (event.key === "Enter" && inputBox.value.trim() !== "") {
+            createNewBoard(inputBox.value, false);
+            reduceCard(createNewBoardCard, event);
+        }
+    });
+
+    closeIcon.addEventListener("click", function(){
+        reduceCard(createNewBoardCard, event);
+    });
+    cancelButton.addEventListener("click", function(){
+        reduceCard(createNewBoardCard, event);
+    });
+    createButton.addEventListener("click", function() {
+        if (inputBox.value.trim() !== "") {
+            createNewBoard(inputBox.value, false);
+            reduceCard(createNewBoardCard, event);
+        }
+    });
+
+    createNewBoardCard.addEventListener("click", toggleCard);
 }
 
 function toggleCard() {
@@ -88,18 +86,19 @@ function expandCard(card) {
     card.appendChild(boardCreationDiv);
 }
 
-function reduceCard(card) {
+function reduceCard(card, e) {
     card.classList.remove("active");
     while (card.firstChild) {
         card.removeChild(card.firstChild);
     }
     card.textContent = "Create a new board...";
+    inputBox.value = "";
+    e.stopPropagation();
 }
 
 function createNewBoard(title, initializingBoards) {
     let newCard = document.createElement("div");
-    newCard.classList.add("card");
-    newCard.classList.add("grow-on-hover");
+    newCard.classList.add("card", "grow-on-hover");
     newCard.textContent = title;
     newCard.addEventListener("click", function() {
         window.location = "./sample-list.html?title=" + title;
@@ -115,9 +114,7 @@ function createNewBoard(title, initializingBoards) {
             };
         }
         savedBoardData.boards.push({ "title": title });
-        localStorage.setItem("savedBoards", JSON.stringify({
-            "boards": savedBoardData.boards
-        }));
+        localStorage.setItem("savedBoards", JSON.stringify(savedBoardData));
     }
 }
 
